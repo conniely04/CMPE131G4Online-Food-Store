77d3c1c010ec92f7989ae10480dcb7a607c83c84
conniely <conniely04@gmail.com>
2023-10-15 01:31:49 -0700

    commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,11 @@
 export const Login = () => {
   return (
     <form>
-      <label></label>
+      <label for="email">Email</label>
       <input type="email" placeholder="youremail@gmail.com" />
+      <br></br>
+      <label for="password">Password</label>
+      <input type="password" placeholder="Enter password" />
     </form>
   );
 };
