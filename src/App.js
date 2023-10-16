ffbb286df9e139dcf96aa6aeb9fcc250dd628df0
conniely <conniely04@gmail.com>
2023-10-15 23:23:37 -0700

    new folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import { Login } from "./Login";
-import { Register } from "./Register";
+import { Login } from "./Components/Login";
+import { Register } from "./Components/Register";
 import React, { useState } from "react";
-import Header from "./Header";
+import Header from "./Components/Header";
 
 function App() {
   const [currentForm, setCurrentForm] = useState("login");
