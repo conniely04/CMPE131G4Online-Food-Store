54114f232ba74acb953165fd426e0b58faf44760
conniely <conniely04@gmail.com>
2023-10-15 19:11:18 -0700

    login page header

---
+++ src/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+function Header() {
+  const [isSticky, setIsSticky] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={isSticky ? "fixed-header" : ""}>
+      <link
+        href="https://fonts.googleapis.com/css?family=Cabin"
+        rel="stylesheet"
+      ></link>
+      <nav>
+        <div className="logo">Online Food Store</div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
