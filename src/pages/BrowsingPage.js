20bb0abee97cb0bdd42962d682cbfadb64e4dff7
“Hiba <hibahasan151214@gmail.com>
2023-10-22 05:12:45 -0700

    updated code - hiba

--- src/pages/BrowsingPage.js
+++ src/pages/BrowsingPage.js
@@ -1,33 +1,34 @@
-import React, { useState } from "react";
-import CategoryItems from "../Components/CategoryItems"; // Import the CategoryItems component
-import "../Components/CategoryItems.css";
+import React, { useState } from 'react';
+import CategoryItems from '../Components/CategoryItems'; // Import the CategoryItems component
+import '../Components/CategoryItems.css';
 
 const BrowsingPage = () => {
   const categoriesData = [
-    { id: 1, name: "Pantry staples", image: "pantry.png" },
-    { id: 2, name: "Vegetables", image: "broccoli2.png" },
-    { id: 3, name: "Fruits", image: "orange.png" },
-    { id: 4, name: "Proteins", image: "meat.png" },
-    { id: 5, name: "Grains", image: "wheat.png" },
-    { id: 6, name: "Beverages", image: "lemonade.png" },
-    { id: 7, name: "Dairy", image: "cheese.png" },
-    { id: 8, name: "Sea Food", image: "seafood.png" },
+    { id: 1, name: 'Pantry staples', image: 'pantry.png' },
+    { id: 2, name: 'Vegetables', image: 'broccoli2.png' },
+    { id: 3, name: 'Fruits', image: 'orange.png' },
+    { id: 4, name: 'Proteins', image: 'meat.png' },
+    { id: 5, name: 'Grains', image: 'wheat.png' },
+    { id: 6, name: 'Beverages', image: 'lemonade.png' },
+    { id: 7, name: 'Dairy', image: 'cheese.png' },
+    { id: 8, name: 'Sea Food', image: 'seafood.png' },
+    // Add more categories as needed
   ];
 
-  const [scrollX, setScrollX] = useState(0);
   const maxVisibleCategories = 6;
-  const maxScrollX = categoriesData.length - maxVisibleCategories; // Maximum scroll position
-
-  const [selectedCategory, setSelectedCategory] = useState("Pantry staples"); // Initial category
+  const [scrollIndex, setScrollIndex] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState(categoriesData[0].name);
 
   const scrollLeft = () => {
-    setScrollX(Math.max(0, scrollX - 2));
+    if (scrollIndex > 0) {
+      setScrollIndex(scrollIndex - 1);
+    }
   };
 
   const scrollRight = () => {
-    const newScrollX = scrollX + 2;
-    setScrollX(Math.min(maxScrollX, scrollX + 2));
-    setScrollX(Math.min(maxScrollX, newScrollX));
+    if (scrollIndex + maxVisibleCategories < categoriesData.length) {
+      setScrollIndex(scrollIndex + 1);
+    }
   };
 
   const handleCategoryClick = (category) => {
@@ -35,60 +36,49 @@ const BrowsingPage = () => {
   };
 
   return (
-    //image-banner
-
-    <div className="whole-page">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Cabin"
-          rel="stylesheet"
-        ></link>
-      </head>
+    <div className="browsing-page">
+      <div className="background-gradient"></div> {/* New background gradient */}
+     
+      {/* image banner */}
       <div className="image-banner">
         <img src="/Frame.png" alt="Shop Now" className="banner-image" />
       </div>
-
-      {/* category-list */}
+      
+      {/* category list */}
       <div className="category-list-container">
         <div className="scroll-arrow left-arrow" onClick={scrollLeft}>
           <img src="/arrow-left.png" alt="Left Arrow" />
         </div>
-        <div className="category-list-container-inner">
-          <div
-            className="category-list"
-            style={{ transform: `translateX(-${scrollX * 200}px)` }}
-          >
-            {categoriesData
-              .slice(scrollX, scrollX + maxVisibleCategories)
-              .map((category) => (
-                <div
-                  key={category.id}
-                  className="category-box"
-                  onClick={() => handleCategoryClick(category.name)} // Add this onClick handler
-                >
-                  <img
-                    src={`/images/${category.image}`}
-                    alt={category.name}
-                    className="category-image"
-                  />
-                  <p className="category-name">{category.name}</p>
-                </div>
-              ))}
-          </div>
+        <div className="category-list">
+          {categoriesData
+            .slice(scrollIndex, scrollIndex + maxVisibleCategories)
+            .map((category) => (
+              <div
+                key={category.id}
+                className={`category-box ${
+                  selectedCategory === category.name ? 'selected' : ''
+                }`}
+                onClick={() => handleCategoryClick(category.name)}
+              >
+                <img
+                  src={`/images/${category.image}`}
+                  alt={category.name}
+                  className="category-image"
+                />
+                <p className="category-name">{category.name}</p>
+              </div>
+            ))}
         </div>
         <div className="scroll-arrow right-arrow" onClick={scrollRight}>
           <img src="/arrow-right.png" alt="Right Arrow" />
         </div>
-        <div className="category-line"></div>{" "}
-        {/* Add this line below the category section */}
       </div>
 
+      <div className="category-line"></div> {/* Add this line to separate the category section from category items */}
+      
       <CategoryItems selectedCategory={selectedCategory} />
     </div>
   );
 };
 
 export default BrowsingPage;
-
-//changes search bar no border
-//change font to mstch
