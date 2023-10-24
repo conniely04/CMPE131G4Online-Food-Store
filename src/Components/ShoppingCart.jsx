ce7d46225c24bab3c2a0d643b1907ff6961c8a50
Alan Vu <alanvu2440@gmail.com>
2023-10-23 19:25:16 -0700

    Changes to Shopping cart

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -2,27 +2,137 @@ import React from "react";
 import "./ShoppingCart.css";
 
 function ShoppingCart({ cart, removeFromCart }) {
+  const handleIncrement = (itemId) => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === itemId) {
+        item.quantity += 1;
+      }
+      return item;
+    });
+    removeFromCart(updatedCart);
+  };
+
+  const handleDecrement = (itemId) => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === itemId && item.quantity > 0) {
+        item.quantity -= 1;
+      }
+      return item;
+    });
+    removeFromCart(updatedCart);
+  };
+
+  // Calculate the total number of items in the cart
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+
+  // Calculate total weight considering quantity
+  const totalWeight = cart.reduce(
+    (total, item) => total + parseFloat(item.weight) * item.quantity,
+    0
+  );
+
+  // Calculate shipping cost based on adjusted total weight
+  const calculateShippingCost = (weight) => {
+    const freeShippingLimit = 20; // Pounds
+    const flatRatePer10Pounds = 5; // Adjust this based on your pricing
+
+    if (weight <= freeShippingLimit) {
+      // No shipping cost
+      return 0;
+    } else {
+      // Calculate the shipping cost for every 10 pounds over the limit
+      const weightOverLimit = weight - freeShippingLimit;
+      const additionalCost =
+        Math.ceil(weightOverLimit / 10) * flatRatePer10Pounds;
+      return additionalCost;
+    }
+  };
+
+  const shippingCost = calculateShippingCost(totalWeight);
+
+  // Define the tax rate (e.g., 8% tax)
+  const taxRate = 0.08;
+
+  // Calculate tax amount based on the total cost of items
+  const taxAmount =
+    cart.reduce((total, item) => total + item.price * item.quantity, 0) *
+    taxRate;
+
   return (
-    <div className="shopping-cart">
-      {cart.length > 0 ? (
-        <ul className="cart-items">
-          {cart.map((item) => (
-            <div className="cart-item" key={item.id}>
-              <img src={item.image} alt={item.name} className="product-image" />
-              <div className="product-details">
-                <span className="product-name">{item.name}</span>
-                <span className="product-quantity">
-                  Quantity: {item.quantity}
-                </span>
-                <span>Cost: ${item.price.toFixed(2)}</span>
+    <div className="shopping-cart-page">
+      <div className="top-half">
+        <div className="top-left">
+          <button className="continue">Continue Shopping</button>
+        </div>
+        <div className="top-center">
+          <h2>Items ({totalItems})</h2>
+        </div>
+        <div className="top-right">
+          <button className="checkout">Checkout</button>
+        </div>
+      </div>
+      <hr className="divider" />
+      <div className="bottom-half">
+        <div className="bottom-left">
+          {cart.length === 0 ? (
+            <p>Your cart is empty</p>
+          ) : (
+            cart.map((item) => (
+              <div key={item.id} className="cart-item">
+                <img src={item.image} alt={item.name} />
+                <div className="item-details">
+                  <p className="item-name">{item.name}</p>
+                  <p className="item-price">Price: ${item.price.toFixed(2)}</p>
+                  <p className="item-quantity">Quantity: {item.quantity}</p>
+                </div>
+                <div className="item-actions">
+                  <button onClick={() => handleIncrement(item.id)}>+</button>
+                  <button onClick={() => handleDecrement(item.id)}>-</button>
+                  <button onClick={() => removeFromCart(item.id)}>
+                    Remove
+                  </button>
+                </div>
               </div>
-              <button onClick={() => removeFromCart(item.id)}>Remove</button>
-            </div>
-          ))}
-        </ul>
-      ) : (
-        <p className="empty-cart-message">Your cart is empty.</p>
-      )}
+            ))
+          )}
+        </div>
+        <div className="bottom-right">
+          <div className="summary-box">
+            <h1>Summary</h1>
+            {cart.length === 0 ? (
+              <p>Your cart is empty</p>
+            ) : (
+              <>
+                <p>
+                  Total Cost: $
+                  {cart
+                    .reduce(
+                      (total, item) => total + item.price * item.quantity,
+                      0
+                    )
+                    .toFixed(2)}
+                </p>
+                <p>Tax: ${taxAmount.toFixed(2)}</p>
+                <p>Total Weight: {totalWeight.toFixed(2)} lbs</p>
+                {totalWeight > 20 && (
+                  <p>Shipping Cost: ${shippingCost.toFixed(2)}</p>
+                )}
+                <p>
+                  Total Cost (including Tax and Shipping): $
+                  {(
+                    cart.reduce(
+                      (total, item) => total + item.price * item.quantity,
+                      0
+                    ) +
+                    taxAmount +
+                    (totalWeight > 20 ? shippingCost : 0)
+                  ).toFixed(2)}
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
