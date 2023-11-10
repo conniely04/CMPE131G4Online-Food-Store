6afe442da39f1ead1b225b1e9288d19431767862
“Hiba <hibahasan151214@gmail.com>
2023-11-09 18:31:10 -0800

    new files Hiba

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import the Link component
+import { Link } from 'react-router-dom';
 import '../Components/Footer.css';
 
 const Footer = () => {
@@ -11,26 +11,25 @@ const Footer = () => {
           <p className="text">We are Open 24 hours</p>
         </div>
         <div className="section">
-        <Link to="/about">
+          <Link to="/about">
             <p className="title">ABOUT US</p>
           </Link>
           <p className="text">Learn more about our Story!</p>
         </div>
         <div className="section">
-        <Link to="/contact">
+          <Link to="/contact">
             <p className="title">LET'S GET SOCIAL !</p>
           </Link>
           <div className="social-icons">
-            <p className="social-link">Instagram</p>
-            <p className="social-link">Facebook</p>
-            <p className="social-link">Twitter</p>
+            <img src="/images/instagram.png" alt="Instagram" className="social-icon" />
+            <img src="/images/twitter.png" alt="Facebook" className="social-icon" />
+            <img src="/images/facebook.png" alt="Twitter" className="social-icon" />
           </div>
-        
-          {/* <div className="footer">
-             <p>Copyright © 2023 Online Food Store. All rights reserved.</p>
-        </div> */}
         </div>
       </div>
+      {/* <div className="footer">
+        <p>Copyright © 2023 Online Food Store. All rights reserved.</p>
+      </div> */}
     </div>
   );
 };
