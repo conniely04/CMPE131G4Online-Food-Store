8cdc843c094f03915aa60c9663ddb3416f92e5ff
“Hiba <hibahasan151214@gmail.com>
2023-11-26 18:35:56 -0800

    errors fixed for checkout hiba

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import LoginPage from "./pages/LoginPage";
 import Broccoli from "./pages/Broccoli";
 import Banana from "./pages/Banana";
 import Carrot from "./pages/Carrot";
-import Checkout from "./Components/Checkout";
+import Checkout from "./pages/Checkout";
 import "./Components/Navbar.css";
 import BrowsingPage from "./pages/BrowsingPage";
 import "./BrowsingPage.css";
